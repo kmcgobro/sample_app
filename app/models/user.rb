edc73621114dd95dc10ac6340a2884af9283afdc
Kyle McGowan <kyle@Pegasus.(none)>
2012-01-15 21:07:02 -0500

    Finished first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email
+	
+	validates :name,
+				:presence => true,
+				:length => {:maximum => 32}
+	
+	validates :email,
+				:presence => true,
+				:format => {:with => /^[a-z]+[\w\d\-.]+@[a-z\d]+\.[a-z]{3}/i},
+				:uniqueness => {:case_sensitive => false}
 end
